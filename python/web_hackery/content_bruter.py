37e9b520bc9e267e3aa857d8867f3f45598e829b
Anthony Nandaa <profnandaa@gmail.com>
2016-01-16 08:41:31 +0300

    (Web) Brute Force directories - completed

--- python/web_hackery/content_bruter.py
+++ python/web_hackery/content_bruter.py
@@ -32,3 +32,50 @@ def built_wordlist(wordist_file):
 			words.put(word)
 
 	return words
+
+def dir_bruter(word_queue, extensions=None):
+	attempt = word_queue.get()
+
+	attempt_list = []
+
+	# check to see if there is a file extension; if not,
+	# it's a directory path we're bruting
+
+	if "." not in attempt:
+		attempt_list.append("/%s/" % attempt)
+	else:
+		attempt_list.append("/%s" % attempt)
+
+	# if we want to bruteforce extensions
+	if extensions:
+		for extension in extensions:
+			attempt_list.append("/%s%s" % attempt, extension)
+
+	# iterate over our list of attempts
+	for brute in attempt_list:
+		url - "%s%s" % (target_url, urllib.quote(brute))
+
+		try:
+			headers = {}
+			headers["User-Agent"] = user_agent
+			r = urllib2.Request(url, headers=headers)
+
+			response = urllib2.urlopen(r)
+
+			if len(response.read()):
+				print "[%d] => %s" % (response.code, url)
+
+		except urllib2.URLError, e:
+			if hasattr(e, 'code') and e.code != 404:
+				print "!!! %d => %s" % (e.code, url)
+
+			pass
+
+word_queue = built_wordlist(wordlist_file)
+extensions = [".php", ".bak". ".orig", ".inc"]
+
+for i in range(threads):
+	t = threading.Thread(target=dir_bruter, args=(word_queue, extensions,))
+	t.start()
+
+
